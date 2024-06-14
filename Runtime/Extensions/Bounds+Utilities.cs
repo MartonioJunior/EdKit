d9cb901311eb7932a4497bd73ca9b8b08ffcffe9
Martônio Júnior <martoniojunior@hotmail.com>
2024-06-14 13:40:52 -0300

    feat(Extensions): Added extensions for Bounds

--- Runtime/Extensions/Bounds+Utilities.cs
+++ Runtime/Extensions/Bounds+Utilities.cs
@@ -14,5 +14,22 @@ namespace MartonioJunior.EdKit
                 delta.z.SafeDivideBy(self.extents.z, delta.z)
             );
         }
+
+        public static Axis SampleAxis(this Bounds self, Vector3 point)
+        {
+            var sampledVector = self.Sample(point);
+
+            return new Axis(sampledVector);
+        }
+
+        public static Bounds WithInfiniteExtents(this Bounds self)
+        {
+            return new Bounds(self.center, Vector3.one * float.PositiveInfinity);
+        }
+
+        public static Bounds WithNoExtents(this Bounds self)
+        {
+            return new Bounds(self.center, Vector3.zero);
+        }
     }
 }
