32e3e0a06b51d43a641ad2969c2c80cb71dfa1ce
Martônio Júnior <martoniojunior@hotmail.com>
2024-06-14 09:21:51 -0300

    feat(Axis): Added Flags support and improved names of None values

--- Runtime/Gestures/Position/Axis.cs
+++ Runtime/Gestures/Position/Axis.cs
@@ -31,7 +31,7 @@ namespace MartonioJunior.EdKit
         {
             return vector.x switch
             {
-                float x when float.IsNaN(x) => X.None,
+                float x when float.IsNaN(x) => X.NoneX,
                 float x when x < deadzone => X.Left,
                 float x when x > deadzone => X.Right,
                 _ => X.Center,
@@ -42,7 +42,7 @@ namespace MartonioJunior.EdKit
         {
             return vector.y switch
             {
-                float y when float.IsNaN(y) => Y.None,
+                float y when float.IsNaN(y) => Y.NoneY,
                 float y when y < deadzone => Y.Below,
                 float y when y > deadzone => Y.Above,
                 _ => Y.Neutral,
@@ -53,7 +53,7 @@ namespace MartonioJunior.EdKit
         {
             return vector.z switch
             {
-                float z when float.IsNaN(z) => Z.None,
+                float z when float.IsNaN(z) => Z.NoneZ,
                 float z when z < deadzone => Z.Back,
                 float z when z > deadzone => Z.Front,
                 _ => Z.Body,
@@ -64,21 +64,21 @@ namespace MartonioJunior.EdKit
     #region Axis.X
     public partial struct Axis
     {
-        public enum X { None = 0, Left = 1, Center = 2, Right = 4 }
+        [Flags] public enum X { NoneX = 0, Left = 1, Center = 2, Right = 4 }
     }
     #endregion
 
     #region Axis.Y
     public partial struct Axis
     {
-        public enum Y { None = 0, Below = 1, Neutral = 2, Above = 4 }
+        [Flags] public enum Y { NoneY = 0, Below = 1, Neutral = 2, Above = 4 }
     }
     #endregion
 
     #region Axis.Z
     public partial struct Axis
     {
-        public enum Z { None = 0, Back = 1, Body = 2, Front = 4 }
+        [Flags] public enum Z { NoneZ = 0, Back = 1, Body = 2, Front = 4 }
     }
     #endregion
 }
