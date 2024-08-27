1f27c6f8d69e7ff61458a2abe7fe64fa4d22a10f
Martônio Júnior <martoniojunior@hotmail.com>
2024-08-27 19:41:19 -0300

    docs(RecognizerBehaviour): Initial documentation for type

--- Runtime/Gestures/Components/RecognizerBehaviour.cs
+++ Runtime/Gestures/Components/RecognizerBehaviour.cs
@@ -9,26 +9,78 @@ namespace MartonioJunior.EdKit
     using GestureEvent = Event<IGesture, List<Event<IPose, Placement>>>;
     #endregion
 
+    /**
+    <summary>
+    Component responsible for identifying poses and gestures out of information in <c>Placement</c> data.
+    <br/>
+    The component is composed of two evaluators: <br/>
+    - <c>GestureEvaluator</c>, which is responsible for identifying gestures out of a list of poses. <br/>
+    - <c>PoseEvaluator</c>, which are responsible for identifying poses out of a <c>Placement</c> instance.
+    </summary>
+    <remarks>
+    To identify poses and gestures from tracked <c>Transform</c>s in a scene, wire the <c>Register</c> method to the <c>onUpdatePlacement</c>
+    event in the <c>TrackerBehaviour</c> component.
+    </remarks>
+    */
     public partial class RecognizerBehaviour
     {
         // MARK: Variables
+        /**
+        <summary>Buffer used to store <c>PoseEvent</c> instances for the evaluation of gestures.</summary>
+        */
         List<PoseEvent> buffer = new();
+        /**
+        <summary>Size of the buffer used to evaluate gestures.</summary>
+        */
         [SerializeField] int bufferSize = 10;
         [Header("Evaluation")]
+        /**
+        <summary>List of <c>ScriptableObject</c>-based gestures to be added to evaluation.</summary>
+        <remarks>Poses stored in the <c>Poses</c> property are also registered to the pose evaluator.</remarks>
+        */
         [SerializeField] List<GestureData> gestures = new();
+        /**
+        <summary>Evaluator for gesture events.</summary>
+        */
         [SerializeField] GestureEvaluator gestureEvaluator = new();
+        /**
+        <summary>Evaluator for pose events.</summary>
+        */
         [SerializeField] PoseEvaluator poseEvaluator = new();
 
         // MARK: Properties
+        /**
+        <inheritdoc cref="buffer"/>
+        */
         public List<PoseEvent> Buffer => buffer;
+        /**
+        <inheritdoc cref="gestureEvaluator"/>
+        */
         public GestureEvaluator GestureEvaluator => gestureEvaluator;
+        /**
+        <inheritdoc cref="poseEvaluator"/>
+        */
         public PoseEvaluator PoseEvaluator => poseEvaluator;
         
         // MARK: Events
+        /**
+        <summary>Event fired when the component recognizes a new gesture was executed.</summary>
+        */
         [SerializeField] UnityEvent<GestureEvent> onGestureRecognized = new();
+        /**
+        <summary>Event fired when the component recognizes a new pose was executed.</summary>
+        */
         [SerializeField] UnityEvent<PoseEvent> onPoseRecognized = new();
 
         // MARK: Methods
+        /**
+        <summary>
+        Evaluates the pose buffer to identify a gesture and, when identified, fires <c>onGestureRecognized</c>.
+        <br/>
+        When a gesture is recognized, the buffer is cleared.
+        </summary>
+        <remarks>Called by this component's <c>FixedUpdate</c> method</remarks>
+        */
         public void EvaluateBuffer()
         {
             if (Peek() is not GestureEvent ge) return;
@@ -36,9 +88,18 @@ namespace MartonioJunior.EdKit
             onGestureRecognized.Invoke(ge);
             buffer.Clear();
         }
-
+        /**
+        <summary>Utility function for determining timestamps for events.</summary>
+        <param name="time">Timestamp to mark the event with. If <c>null</c>, the current time is used.</param>
+        <returns>Resolved timestamp.</returns>
+        */
         private float ResolveTime(float? time) => time ?? Time.time;
-
+        /**
+        <summary>Registers a new placement instance identified at a moment in time, firing <c>onPoseRecognized</c> when a pose is successfully detected.</summary>
+        <param name="placement">Placement instance to be registered.</param>
+        <param name="time">Timestamp for the event. If <c>null</c>, the current time is used.</param>
+        <remarks>If the buffer is already full, the operation removes the oldest event registered in the buffer.</remarks>
+        */
         public void Register(Placement placement, float? time = null)
         {
             if (buffer.Count >= bufferSize) {
@@ -50,7 +111,10 @@ namespace MartonioJunior.EdKit
             buffer.Add(pe);
             onPoseRecognized.Invoke(pe);
         }
-
+        /**
+        <summary>Configures the pose and gesture evaluators for the component.</summary>
+        <remarks>Called by the component's <c>Awake</c> method</remarks>
+        */
         public void SetupEvaluators()
         {
             gestures.ForEach(gestureEvaluator.Register);
@@ -62,7 +126,11 @@ namespace MartonioJunior.EdKit
                 return poseList;
             }
         }
-
+        /**
+        <summary>Returns the gesture event identified by the gesture evaluator.</summary>
+        <param name="time">Timestamp for the event. If <c>null</c>, the current time is used.</param>
+        <returns>Gesture event identified by the gesture evaluator.</returns>
+        */
         public GestureEvent? Peek(float? time = null)
         {
             return gestureEvaluator.Evaluate(buffer, ResolveTime(time));
@@ -85,10 +153,18 @@ namespace MartonioJunior.EdKit
     }
     #endregion
 
-    #region UnityEvent Integration
+    #region UnityEvent Bindings
     public partial class RecognizerBehaviour
     {
+        /**
+        <inheritdoc cref="Register"/>
+        */
         public void Register(Placement placement) => Register(placement, null);
+        /**
+        <summary>Registers a new gesture with the gesture evaluator.</summary>
+        <param name="gesture">Gesture to be registered.</param>
+        <remarks>Poses stored in the <c>Poses</c> property are also registered to the pose evaluator.</remarks>
+        */
         public void RegisterGesture(GestureData gesture)
         {
             gestureEvaluator.Register(gesture);
@@ -97,7 +173,14 @@ namespace MartonioJunior.EdKit
                 poseEvaluator.Register(pose);
             }
         }
-
+        /**
+        <summary>Unregisters a gesture from the gesture evaluator.</summary>
+        <param name="gesture">Gesture to be removed.</param>
+        <remarks>
+        This only de-registers the gesture from the evaluation process and not the poses that compose it, as they can be shared between gestures.<br/>
+        To also remove the poses associated with this gesture, use the <c>Unregister</c> method in the <c>PoseEvaluator</c> property.
+        </remarks>
+        */
         public void UnregisterGesture(GestureData gesture)
         {
             gestureEvaluator.Unregister(gesture);
