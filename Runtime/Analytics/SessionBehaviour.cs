4f18ce783716f1a81d6661dc1159a5cacfe6c987
Martônio Júnior <martoniojunior@hotmail.com>
2024-08-29 11:39:58 -0300

    docs(SessionBehaviour): Initial documentation for type

--- Runtime/Analytics/SessionBehaviour.cs
+++ Runtime/Analytics/SessionBehaviour.cs
@@ -8,37 +8,65 @@ namespace MartonioJunior.EdKit
     using UID = System.String;
     #endregion
 
+    /**
+    <summary>Component that manages the lifecycle for a session in a scene.</summary>
+    */
     public partial class SessionBehaviour
     {
         // MARK: Variables
+        /**
+        <summary>Session currently managed by the component.</summary>
+        */
         Session? session = null;
 #if UNITY_EDITOR
+        /**
+        <summary>When set to <c>true</c>, saves the log when leaving play mode.</summary>
+        */
         [Header("Editor Settings")]
         [SerializeField, Tooltip("Saves log when leaving Play Mode")] bool saveLogInEditor = true;
 #endif
 
         // MARK: Events
+        /**
+        <summary>Event invoked when a session is opened.</summary>
+        */
         [SerializeField] UnityEvent<Session> onSessionOpened = new();
+        /**
+        <summary>Event invoked when a session is closed.</summary>
+        */
         [SerializeField] UnityEvent<Session> onSessionClosed = new();
 
         // MARK: Methods
+        /**
+        <summary>Initializes a new session to be managed by the component.</summary>
+        <param name="userID">User ID associated with the session.</param>
+        <param name="sceneID">Scene ID associated with the session.</param>
+        */
         public void OpenSession(UID userID, UID sceneID)
         {
             var s = new Session(userID, sceneID);
             session = s;
             onSessionOpened.Invoke(s);
         }
-
+        /**
+        <summary>Disposes of the current running session.</summary>
+        */
         public void ClearSession()
         {
             session = null;
         }
-
+        /**
+        <param name="outcome">Description of the outcome for the session.</param>
+        <inheritdoc cref="CloseSession(IDictionary{string,object})"/>
+        */
         public void CloseSession(string outcome)
         {
             CloseSession(new Dictionary<string, object> { { "description", outcome } });
         }
-
+        /**
+        <summary>Closes the current session with a given outcome.</summary>
+        <param name="outcome">Dictionary with data about the outcome for the session.</param>
+        */
         public void CloseSession(IDictionary<string, object> outcome)
         {
             if (session is not Session s) return;
@@ -46,8 +74,13 @@ namespace MartonioJunior.EdKit
             s.RegisterOutcome(outcome);
             SaveToLog(s);
             onSessionClosed.Invoke(s);
+            ClearSession();
         }
-
+        /**
+        <summary>Saves a session into a log file.</summary>
+        <param name="session">Session to be saved.</param>
+        <remarks>In the Unity Editor, the log will not be saved when <c>saveLogInEditor</c> property is set to <c>false</c>.</remarks>
+        */
         private void SaveToLog(Session session)
         {
 #if UNITY_EDITOR
@@ -65,8 +98,17 @@ namespace MartonioJunior.EdKit
     #region IProvenance Implementation
     public partial class SessionBehaviour: IProvenanceModel
     {
+        /**
+        <inheritdoc cref="IProvenanceModel.Register(Activity)"/>
+        */
         public void Register(Activity activity) => session?.Register(activity);
+        /**
+        <inheritdoc cref="IProvenanceModel.Register(Agent)"/>
+        */
         public void Register(Agent agent) => session?.Register(agent);
+        /**
+        <inheritdoc cref="IProvenanceModel.Register(Entity)"/>
+        */
         public void Register(Entity entity) => session?.Register(entity);
     }
     #endregion
