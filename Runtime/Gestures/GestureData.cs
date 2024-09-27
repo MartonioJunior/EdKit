460bc7a9081d9bb07f44e116a30bd7a198b2f72b
Martônio Júnior <martoniojunior@hotmail.com>
2024-09-27 14:02:49 -0300

    change(GestureData): Changed evaluation algorithm
    Score is now defined by the lowest scoring pose and requires all poses to be identified at some level

--- Runtime/Gestures/GestureData.cs
+++ Runtime/Gestures/GestureData.cs
@@ -41,18 +41,22 @@ namespace MartonioJunior.EdKit
         */
         public float Evaluate(IList<Event<IPose, Placement>> events)
         {
+            if (poses == null) return 0;
+
             float numberOfPoses = poses.Length + 1;
             var poseScore = 0f;
             var poseIndex = 1;
 
             for (int i = 1; i < numberOfPoses; i++) {
-                if (events[^i].Object.Equals(poses[^poseIndex])) continue;
+                if (events.Count <= i) return 0;
 
-                poseScore += events[^i].Score;
+                if (!events[^i].Object.Equals(poses[^poseIndex])) continue;
+
+                poseScore = Mathf.Min(poseScore, events[^i].Score);
                 poseIndex++;
             }
 
-            return poseScore / numberOfPoses;
+            return poseScore;
         }
     }
     #endregion
